f00b8d2fddfc73db41f3a7c5f920a41d90b3ee74
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-03-31 16:58:23 +0100

    Connect the app to api
    Used middleware and thunk to connect the app with api

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadBook } from '../redux/books/books';
 import Book from './Book';
 import AddBook from './AddBook';
 
 const BookList = () => {
+  const dispatch = useDispatch();
+
   const books = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(loadBook());
+  }, []);
 
   return (
     <>
